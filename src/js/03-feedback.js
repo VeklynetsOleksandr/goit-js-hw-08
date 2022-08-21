25db05a0a9b0ca11459164376badc9dba0e9878d
VeklynetsOleksandr <98841842+VeklynetsOleksandr@users.noreply.github.com>
2022-08-21 12:23:16 +0300

    Fix bugs Part 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,35 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
-const emailRef = document.querySelector('input[name="email"]');
-const messageRef = document.querySelector('textarea[name="message"]');
-const dataBase = {
-  email: '',
-  message: '',
-};
 const STORAGE_KEY = 'feedback-form-state';
 
-// --------------------------СЛУХАЧІ ПОДІЇ-------------------------------------
-formRef.addEventListener('submit', submitForm);
 formRef.addEventListener('input', throttle(validateForm, 500));
+formRef.addEventListener('submit', submitForm);
 
-// --------------------------ФУНКЦІЇ-------------------------------------
-function validateForm(e) {
-  const { name, value } = e.target;
-  dataBase[name] = value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataBase));
-}
-function submitForm(e) {
-  e.preventDefault();
-  e.currentTarget.reset();
-  console.log(dataBase);
-}
-function getDataBase() {
-  const getData = localStorage.getItem(STORAGE_KEY);
-  const parsedData = JSON.parse(getData);
-  if (parsedData) {
-    emailRef.value = parsedData.email;
-    messageRef.value = parsedData.message;
-  }
-}
+let dataBase = {};
 
 getDataBase();
+
+function submitForm(evt) {
+  evt.preventDefault();
+  console.log(dataBase);
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function validateForm(evt) {
+  dataBase[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataBase));
+}
+
+function getDataBase() {
+  let savedLocalStorageValue = localStorage.getItem(STORAGE_KEY);
+
+  if (savedLocalStorageValue) {
+    savedLocalStorageValue = JSON.parse(savedLocalStorageValue);
+    Object.entries(savedLocalStorageValue).forEach(([name, value]) => {
+      dataBase[name] = value;
+      formRef.elements[name].value = value;
+    });
+  }
+}
